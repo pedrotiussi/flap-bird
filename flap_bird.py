2aa67feacfc427b3824bbaa65829da3a3a88168d
Pedro Tiussi <pedrotiussi@gmail.com>
2020-12-07 19:44:51 -0300

    Making small changes

--- flap_bird.py
+++ flap_bird.py
@@ -15,7 +15,6 @@ BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bi
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
-
 STAT_FONT = pygame.font.SysFont('comicsans', 50)
 
 class Bird:
